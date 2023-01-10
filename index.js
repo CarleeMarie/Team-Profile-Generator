2d602cec32a64b780d9d363707d4653585193bf0
Carlee Miller <carlee.miller@gmail.com>
2023-01-10 15:58:12 -0600

    saving with more organized code and folders

--- index.js
+++ index.js
@@ -1,38 +1,11 @@
+// this file runs the application
+
 const inquirer = require('inquirer');
 
 //TODO use constructor functions to create objects that contain properties of employees
 
 
-//TODO set variables for each type of employee and employee objects with desired properties 
 // need an action trigger - click on email in the HTML - to start the command line questions
-
-// properties needed
-//  email address or the employee
-//  GitHub username
-//  1. team manager's name
-//  2. employee ID
-//  3. email address
-//  4. office number
-
-function Employee(name, id, email) {
-    this.name = name;
-    this.id = id;
-    this.email = email;
-    this.getName();
-    this.getID();
-    this.getEmail();
-    this.getRole();
-}
-
-function Manager(officeNumber) {
-    //overide getRole above to return Engineer
-
-}
-
-function Intern(school) {
-    this.getSchool();
-    // this.getRole(); override to return Intern
-}
 
 const manager = new Employee();
 const intern = new Employee();
